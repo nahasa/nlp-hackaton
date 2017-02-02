7939b767204a629bb395432bca77d3136e09f63b
J. Ricardo de Juan Cajide <jricardo.dejuan@gmail.com>
2017-02-02 12:30:51 +0100

    uploading

--- hackaton_grupo3/config.py
+++ hackaton_grupo3/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Edit this to find dataset parent folder
-BASE_PATH = '/Users/nahasa/Work/workshop/nlu/nlp-hackaton/hackaton_grupo3'
+BASE_PATH = '/home/josedejuan/NLP/hackathon/nlp-hackaton/hackaton_grupo3'
 
 # Datasets
 DATASET_TED_RAW = BASE_PATH+'/dataset/TED_talks'
