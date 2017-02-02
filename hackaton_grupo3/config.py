0bf4f5be139b4531b0380df7ff7b76be716f0533
Arturo García Rodríguez <nahasa@nahasa-2.local>
2017-02-02 10:55:43 +0100

    Full commit

--- hackaton_grupo3/config.py
+++ hackaton_grupo3/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Edit this to find dataset parent folder
-BASE_PATH = '/home/luismesa/repo/workshop-nlp'
+BASE_PATH = '/Users/nahasa/Work/workshop/nlu/nlp-hackaton/hackaton_grupo3'
 
 # Datasets
-DATASET_TED_RAW = BASE_PATH+'/datasets/TED_talks'
+DATASET_TED_RAW = BASE_PATH+'/dataset/TED_talks'
